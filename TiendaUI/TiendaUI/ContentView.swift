94fe430a90f029c011545775f1282a2bb82bea5b
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-02-19 20:30:20 +0100

    Add files via upload

---
+++ TiendaUI/TiendaUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TiendaUI
+//
+//  Created by dam2 on 19/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TiendaUI/TiendaUI/TiendaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiendaUIApp.swift
+//  TiendaUI
+//
+//  Created by dam2 on 19/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TiendaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
